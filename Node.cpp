513e30a52e27d224ff5ddd676e5719f30b0ee735
Miomit <movsesyan-03@inbox.ru>
2024-03-22 14:32:08 +0400

    added hash code in Node

--- Node.cpp
+++ Node.cpp
@@ -1,4 +1,5 @@
 #include "Node.hpp"
+#include "MySTL.hpp"
 
 #include <cstringt.h>
 
@@ -6,4 +7,6 @@ gne::Node::Node(char name[50], char label[50])
 {
 	strcpy_s(this->_name, sizeof(this->_name), name);
 	strcpy_s(this->_label, sizeof(this->_label), label);
+
+	this->_hashCode = my_stl::str_to_hash(name);
 }
--- Node.hpp
+++ Node.hpp
@@ -5,9 +5,12 @@ namespace gne
 	public:
 		Node(char name[50], char label[50]);
 
-		char* getName() { return _name; };
-		char* getLabel() { return _label; };
+		char* getName() { return _name; }
+		char* getLabel() { return _label; }
+
+		int getHashCode() { return _hashCode; }
 	private:
+		int _hashCode;
 		char _name[50], _label[50];
 	};
 }
