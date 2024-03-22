aa974dd17894011b41162079f9f1c7801c0fc2fc
Miomit <movsesyan-03@inbox.ru>
2024-03-22 18:10:27 +0400

    test Edge

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
-#include "Node.hpp"
+#include "Edge.hpp"
 
 int main()
 {
 	char name[50] = "A";
-	auto a = gne::Node(name);
+	auto a = new gne::Node(name);
 	name[0] = 'B';
-	auto b = gne::Node(name);
-	auto c = gne::Node(name);
+	auto b = new gne::Node(name);
+	auto c = new gne::Node(name);
 
-	std::cout << a << "; hash = " << a.getHashCode() << std::endl;
-	std::cout << b << "; hash = " << b.getHashCode() << std::endl;
-	std::cout << c << "; hash = " << c.getHashCode() << std::endl;
+	std::cout << *a << "; hash = " << a->getHashCode() << std::endl;
+	std::cout << *b << "; hash = " << b->getHashCode() << std::endl;
+	std::cout << *c << "; hash = " << c->getHashCode() << std::endl;
+
+	name[0] = 'E';
+
+	auto k = gne::Edge(a, b, gne::ORIENTED, name);
+	auto k2 = gne::Edge(b, a, gne::ORIENTED, name);
+
+
+	std::cout << k << "; hash = " << k.getHashCode() << std::endl;
+	std::cout << k2 << "; hash = " << k2.getHashCode() << std::endl;
 	
 	return 0;
 }
