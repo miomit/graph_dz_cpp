9f6aac3f8edbce15949400b49095044cd8cb855f
Miomit <movsesyan-03@inbox.ru>
2024-03-23 14:56:36 +0400

    update cmd

--- Main.cpp
+++ Main.cpp
@@ -23,48 +23,104 @@ void graph_to_png(gne::Graph* graph)
 int main()
 {
 	char buff[50];
-	int num_tokens;
+	char buff2[50];
+	char buff3[50];
+
+	char cmd[50];
 	int n;
 	gne::Graph* graph;
+	gne::TypeEdge typeGraph{};
 	
 	std::cout << "1) digraph\n" << "2) graph\n> ";
 	std::cin >> n;
 
-	if (n % 2 == 0)
-		graph = new gne::Graph(gne::ORIENTED);
-	else
-		graph = new gne::Graph(gne::UNORIENTED);
+	if (n % 2 == 0) typeGraph = gne::ORIENTED;
+	else typeGraph = gne::UNORIENTED;
+
+	graph = new gne::Graph(typeGraph);
 
 	while (true)
 	{
 		graph_to_png(graph);
 
-		std::cout << "> ";
-		std::cin >> buff;
-
-		char** tokens = my_stl::split(buff, '_', &num_tokens);
-		
-		if (num_tokens == 2) 
+		std::cout << "(1-4): ";
+		std::cin >> n;
+		std::cout << "$ ";
+		if (n == 1)
 		{
-			if (tokens[0][0] == '+')
+			std::cin >> cmd >> buff;
+		}else
+		if (n == 2)
+		{
+			std::cin >> cmd >> buff >> buff2;
+		}
+		else if (n == 3)
+		{
+			std::cin >> cmd >> buff >> buff2 >> buff3;
+		}
+		else
+		{
+			continue;
+		}
+		
+		if (n == 1) 
+		{
+			if (cmd[0] == '+')
 			{
-				strcpy_s(buff, sizeof(buff), tokens[1]);
 				auto node = new gne::Node(buff);
 				if (!graph->add(node)) 
 				{
 					std::cout << "such a node exists" << std::endl;
 					delete node;
 				}
-			} else if (tokens[0][0] == '-')
+			} else if (cmd[0] == '-')
 			{
-				strcpy_s(buff, sizeof(buff), tokens[1]);
 				auto node = new gne::Node(buff);
 				graph->remove(node);
 				delete node;
 			}
 		}
+		else if (n == 2)
+		{
+			if (cmd[0] == '-')
+			{
+				auto node1 = new gne::Node(buff);
 
-		delete[] tokens;
+				auto node2 = new gne::Node(buff2);
+
+				strcpy_s(buff, sizeof(buff), "");
+				auto edge = new gne::Edge(node1, node2, typeGraph, buff);
+
+				if (!graph->add(edge))
+				{
+					std::cout << "such a edge exists" << std::endl;
+					delete edge;
+				}
+
+				delete node1;
+				delete node2;
+			}
+		} 
+		else if (n == 3)
+		{
+			if (cmd[0] == '-')
+			{
+				auto node1 = new gne::Node(buff);
+
+				auto node2 = new gne::Node(buff2);
+
+				auto edge = new gne::Edge(node1, node2, typeGraph, buff3);
+
+				if (!graph->add(edge))
+				{
+					std::cout << "such a edge exists" << std::endl;
+					delete edge;
+				}
+
+				delete node1;
+				delete node2;
+			}
+		}
 	}
 
 	return 0;
